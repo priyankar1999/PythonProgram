75e0da30e89326c9436cab1664c1b282b34ba330
priyankar1999 <150575867+priyankar1999@users.noreply.github.com>
2023-11-12 12:45:47 +0530

    Update test_orders.py

--- test_orders.py
+++ test_orders.py
@@ -1,12 +1,12 @@
 import unittest
 import os
 
-from order_processing import monthly_revenue, product_revenue, customer_revenue, top_customers
+from orders import monthly_revenue, product_revenue, customer_revenue, top_customers
 
 class TestOrderProcessing(unittest.TestCase):
 
     def test_monthly_revenue(self):
-        script_path = 'order_processing.py'
+        script_path = 'orders.py'
         csv_path = 'orders.csv'
 
         os.system(f'python {script_path}')
@@ -29,10 +29,8 @@ class TestOrderProcessing(unittest.TestCase):
         # Check if the total revenue for May 2023 is calculated correctly
         self.assertAlmostEqual(monthly_revenue['2023-05'], 90.00, places=2)
 
-        # Add more assertions based on your specific data and calculations
-
     def test_product_revenue(self):
-        script_path = 'order_processing.py'
+        script_path = 'orders.py'
         csv_path = 'orders.csv'
 
         os.system(f'python {script_path}')
@@ -48,11 +46,9 @@ class TestOrderProcessing(unittest.TestCase):
 
         # Check if the total revenue for Product_C is calculated correctly
         self.assertAlmostEqual(product_revenue['Product_C'], 150.00, places=2)
-
-        # Add more assertions based on your specific data and calculations
-
+        
     def test_customer_revenue(self):
-        script_path = 'order_processing.py'
+        script_path = 'orders.py'
         csv_path = 'orders.csv'
 
         os.system(f'python {script_path}')
@@ -69,10 +65,8 @@ class TestOrderProcessing(unittest.TestCase):
         # Check if the total revenue for Customer_3 is calculated correctly
         self.assertAlmostEqual(customer_revenue['Customer_3'], 300.00, places=2)
 
-        # Add more assertions based on your specific data and calculations
-
     def test_top_customers(self):
-        script_path = 'order_processing.py'
+        script_path = 'orders.py'
         csv_path = 'orders.csv'
 
         os.system(f'python {script_path}')
@@ -83,8 +77,6 @@ class TestOrderProcessing(unittest.TestCase):
         # Check if the top customer is calculated correctly
         self.assertEqual(top_customers[0][0], 'Customer_1')
         self.assertAlmostEqual(top_customers[0][1], 330.00, places=2)
-
-        # Add more assertions based on your specific data and calculations
 
 if __name__ == '__main__':
     unittest.main()
