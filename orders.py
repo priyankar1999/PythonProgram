8731c946049a15592487d3b4244179f13d394cde
priyankar1999 <150575867+priyankar1999@users.noreply.github.com>
2023-11-12 18:54:17 +0530

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 from datetime import datetime
+import os
 
 # Define the CSV file path
 csv_file = 'orders.csv'
@@ -13,38 +14,34 @@ customer_revenue = defaultdict(float)
 # Read the CSV file and process data
 required_columns = ['order_date', 'customer_id', 'product_name', 'product_price', 'quantity']
 
-# Read the CSV file and process data
 try:
     with open(csv_file, 'r') as file:
         reader = csv.DictReader(file)
 
         # Check if all required columns are present
         if not all(column in reader.fieldnames for column in required_columns):
-            print("Missing required columns in the CSV file.")
+            print(f"Missing required columns in the CSV file. Expected columns: {', '.join(required_columns)}")
             exit(1)
 
         for row in reader:
             order_date = datetime.strptime(row['order_date'], '%Y-%m-%d')
-            month_key = f'{order_date.year}-{order_date.month:02d}'
-            
+
             product_price = float(row['product_price'])
             quantity = int(row['quantity'])
-            
+
             # Compute monthly revenue
+            month_key = f'{order_date.year}-{order_date.month:02d}'
             monthly_revenue[month_key] += product_price * quantity
-            
+
             # Compute product revenue
             product_revenue[row['product_name']] += product_price * quantity
-            
+
             # Compute customer revenue
             customer_revenue[row['customer_id']] += product_price * quantity
 
 except FileNotFoundError:
     print(f"File {csv_file} not found.")
     exit(1)
-
-# Compute the top 10 customers by revenue
-top_customers = sorted(customer_revenue.items(), key=lambda x: x[1], reverse=True)[:10]
 
 # Print results
 for month, revenue in monthly_revenue.items():
@@ -55,6 +52,9 @@ for product, revenue in product_revenue.items():
 
 for customer, revenue in customer_revenue.items():
     print(f"Revenue for Customer {customer}: ${revenue:.2f}")
+
+# Identify the top 10 customers by revenue
+top_customers = sorted(customer_revenue.items(), key=lambda x: x[1], reverse=True)[:10]
 
 print("Top 10 Customers by Revenue:")
 for i, (customer, revenue) in enumerate(top_customers, start=1):
